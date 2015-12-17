97e3d0f776d7ea808c626f8e0600d6a86f6f5030
xcvbn2004 <xcvbn2004@gmail.com>
2015-12-18 04:42:05 +0800

    add isShow

--- fyp_db.sql
+++ fyp_db.sql
@@ -54,14 +54,14 @@ CREATE TABLE `food`(
 	`qty` integer NOT NULL,
 	`dQty` integer NOT NULL,
 	`fTypeId`  varchar(10) NOT NULL,
-	`isShow` varchar(1) NOT NULL,
+	`isShow` varchar(1) NOT NULL DEFAULT 'Y',
 	PRIMARY KEY(`foodId`)
 ) CHARACTER SET = utf8;
 CREATE TABLE `orderFood`(
 	`orderDate` date NOT NULL,
 	`orderId` varchar(10) NOT NULL,
 	`foodId` varchar(10) NOT NULL,
-	`price` numeric(5,2)
+	`price` numeric(5,2) NOT NULL
 ) CHARACTER SET = utf8;
 CREATE TABLE `menu`(
 	`menuId` varchar(10) NOT NULL,
@@ -69,7 +69,7 @@ CREATE TABLE `menu`(
 	`price` numeric(5,2) NULL,
 	`img` varchar(50) NULL,
 	`mCateId` varchar(10) NOT NULL,
-	`isShow` varchar(1) NOT NULL,
+	`isShow` varchar(1) NOT NULL DEFAULT 'Y',
 	PRIMARY KEY(`menuId`)
 ) CHARACTER SET = utf8;
 CREATE TABLE `menuFood`(
@@ -81,6 +81,7 @@ CREATE TABLE `menuCategory`(
 	`mCateId` varchar(10) NOT NULL,
 	`img` varchar(50) NULL,
 	`name` varchar(50) NOT NULL,
+	`isShow` varchar(1) NOT NULL DEFAULT 'Y',
 	PRIMARY KEY(`mCateId`)
 ) CHARACTER SET = utf8;
 CREATE TABLE `foodType`(
@@ -126,28 +127,28 @@ INSERT INTO `foodType`(`fTypeId`, `name`) VALUES
 ('FT00000003', 'Batching'),
 ('FT00000004', 'siu mei Batching')
 ;
-INSERT INTO `food` (`foodId`, `name`, `price`, `sPrice`, `qty`, `dQty`, `fTypeId`, `isShow`) VALUES
-('F00000001', '香濃咖喱牛腩飯(Fragrant curry beef brisket rice)', 30, 0, 100, 100, 'FT00000001', 'Y'),
-('F00000002', '香濃咖喱雞飯(Fragrant curry chicken)', 30, 0, 100, 100, 'FT00000001', 'Y'),
-('F00000003', '即磨咖啡(ground coffee)', 12, 0, 100, 100, 'FT00000002', 'Y'),
-('F00000004', '凍檸茶(Ice lemon tea)', 15, 3, 100, 100, 'FT00000002', 'Y'),
-('F00000005', '紅豆冰(Red bean ice)', 18, 8, 100, 100, 'FT00000002', 'Y'),
-('F00000006', '叉燒(Barbecue pork)', 30, 0, 100, 100, 'FT00000004', 'Y'),
-('F00000007', '燒鴨(Roast duck)', 30, 0, 100, 100, 'FT00000004', 'Y'),
-('F00000008', '燒肉(Roasted pork)', 30, 0, 100, 100, 'FT00000004', 'Y'),
-('F00000009', '切雞(Cut chicken)', 30, 0, 100, 100, 'FT00000004', 'Y')
+INSERT INTO `food` (`foodId`, `name`, `price`, `sPrice`, `qty`, `dQty`, `fTypeId`) VALUES
+('F00000001', '香濃咖喱牛腩飯(Fragrant curry beef brisket rice)', 30, 0, 100, 100, 'FT00000001'),
+('F00000002', '香濃咖喱雞飯(Fragrant curry chicken)', 30, 0, 100, 100, 'FT00000001'),
+('F00000003', '即磨咖啡(ground coffee)', 12, 0, 100, 100, 'FT00000002'),
+('F00000004', '凍檸茶(Ice lemon tea)', 15, 3, 100, 100, 'FT00000002'),
+('F00000005', '紅豆冰(Red bean ice)', 18, 8, 100, 100, 'FT00000002'),
+('F00000006', '叉燒(Barbecue pork)', 30, 0, 100, 100, 'FT00000004'),
+('F00000007', '燒鴨(Roast duck)', 30, 0, 100, 100, 'FT00000004'),
+('F00000008', '燒肉(Roasted pork)', 30, 0, 100, 100, 'FT00000004'),
+('F00000009', '切雞(Cut chicken)', 30, 0, 100, 100, 'FT00000004')
 ;
 INSERT INTO `menuCategory`(`mCateId`, `name`) VALUES
 ('MC00000001', '正價(Regular price)'),
 ('MC00000002', '飯類(Rice Category)'),
 ('MC00000003', '燒味(siu mei)')
 ;
-INSERT INTO `Menu` (`menuId`, `name`, `price`, `mCateId`, `isShow`) VALUES
-('M00000001', '飯類(Rice Category)', null, 'MC00000001', 'Y'),
-('M00000002', '飲品(Drink Category)', null, 'MC00000001', 'Y'),
-('M00000003', '套餐(Set rice)', 30, 'MC00000002', 'Y'),
-('M00000004', '雙餸燒味飯(siu mei rice with two choices of sides)', 25, 'MC00000003', 'Y'),
-('M00000005', '雙餸燒味飯套餐(Set of siu mei rice with two choices of sides)', 30, 'MC00000003', 'Y')
+INSERT INTO `Menu` (`menuId`, `name`, `price`, `mCateId`) VALUES
+('M00000001', '飯類(Rice Category)', null, 'MC00000001'),
+('M00000002', '飲品(Drink Category)', null, 'MC00000001'),
+('M00000003', '套餐(Set rice)', 30, 'MC00000002'),
+('M00000004', '雙餸燒味飯(siu mei rice with two choices of sides)', 25, 'MC00000003'),
+('M00000005', '雙餸燒味飯套餐(Set of siu mei rice with two choices of sides)', 30, 'MC00000003')
 ;
 INSERT INTO `menuFood` (`menuId`, `fTypeId`, `many`) VALUES
 ('M00000001', 'FT00000001', 1),
@@ -160,6 +161,8 @@ INSERT INTO `menuFood` (`menuId`, `fTypeId`, `many`) VALUES
 ;
 -- end add data --
 
+-- add test image --
+UPDATE `menuCategory` SET `img`='image/test.png' WHERE `mCateId`='MC00000001';
 
 --get menu--
 SELECT M.menuId,
@@ -168,5 +171,5 @@ M.price,
 M.img,
 GROUP_CONCAT(CONCAT(' ',FT.name,' x ',MF.many)) 'content' 
 FROM menu M, menufood MF, foodtype FT
-WHERE M.menuId=MF.menuId AND MF.fTypeId=FT.fTypeId
+WHERE M.menuId=MF.menuId AND MF.fTypeId=FT.fTypeId AND M.isShow='Y'
 GROUP BY M.menuId;
